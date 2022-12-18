9e92c856d812509ad2f2716d6a9b8bfa7aa68ff6
Argosendy <120897418+Argosendy@users.noreply.github.com>
2022-12-18 13:40:34 +0700

    Add files via upload

---
+++ sendy argono putra (1970231121)/Pertemuan 1/Latihan4.py
@@ -0,0 +1,21 @@
+print(True)
+
+print("Ayo belajar Python")
+print("Belajar Python Sangat Mudah")
+
+print(20)
+
+print(3.14)
+
+print(5j)
+
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+print((1,2,3,4,5))
+print(("satu","dua","tiga"))
+
+print({"nama":"Budi",'umur':20})
+biodata= {"nama":"andi",'Umur':20}
+print(biodata)
+print(type(biodata))
